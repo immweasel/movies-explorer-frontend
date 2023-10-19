58be9034232c4d142db7060574d8e94485796ecf
Кира Максимова 211-323 <immweasel@yandex.ru>
2023-10-19 07:37:26 +0300

    feat: сделать preloader

--- src/components/Preloader/Preloader.jsx
+++ src/components/Preloader/Preloader.jsx
@@ -1,9 +1,13 @@
 import React from 'react';
+import './Preloader.css';
 
 const Preloader = () => {
   return (
-    <div>
+    <div className="preloader">
       <p>Preloader</p> — отвечает за работу прелоадера.
+      <div className="preloader__container">
+        <span className="preloader__round"></span>
+      </div>
     </div>
   );
 }
